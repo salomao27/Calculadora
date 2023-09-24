816845bc4c48adaf59fb3aa12181d65c1381dbc9
Joao Marcos <101514552+salomao27@users.noreply.github.com>
2023-09-24 11:10:27 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,15 @@
+function insert(num){
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}//concatenar
+function clean(){
+    document.getElementById('resultado').innerHTML = "";
+}
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }else{
+        document.getElementById('resultado').innerhtml = "Nada..."
+    }
+}
